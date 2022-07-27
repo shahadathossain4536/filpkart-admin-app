b5fe3af64cb7e4822de7d8584223e9bec9c28c23
shahadat4536 <shahadathossain4536@gmail.com>
2022-07-27 11:21:17 +0600

    admin-redux

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -2,19 +2,32 @@ import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Layout } from "../../components/Layout";
 import { Input } from "../../components/UI/Input";
-
+import { login } from "../../actions";
+import { useDispatch } from "react-redux";
 /**
  * @author
  * @function Signin
  **/
 
 export const Signin = (props) => {
+  const dispatch = useDispatch();
+
+  const userLogin = (e) => {
+    e.preventDefault();
+    const user = {
+      email: "shahadat@gmail.com",
+      password: "123456",
+    };
+
+    dispatch(login(user));
+  };
+
   return (
     <Layout>
       <Container>
         <Row>
           <Col className="my-5" md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={userLogin}>
               <Input
                 label="Email"
                 placeholder="Email"
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+window.store = store;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </BrowserRouter>
 );
 
